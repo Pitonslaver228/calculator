e708f3a501147cf7a9ba118a9131a818dbfbd918
Pitonslaver228 <gaibatovrenat@yandex.ru>
2024-11-05 22:17:14 +0300

    Create main.py
    first calculator version

---
+++ main.py
@@ -0,0 +1,50 @@
+class Calculator:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls.__instance:
+            cls.__instance = True
+            return super().__new__(cls)
+
+    def __init__(self):
+        self.command_flag = None
+
+    @staticmethod
+    def add(x, y):
+        return x + y
+
+    @staticmethod
+    def subtract(x, y):
+        return x - y
+
+    @staticmethod
+    def multiple(x, y):
+        return x * y
+
+    @staticmethod
+    def divide(x, y):
+        return "Ошибка. Деление на 0 не допустимо." if y == 0 else x / y
+
+    def run(self):
+        while self.command_flag != 0:
+
+            val1, val2 = map(int, input("Введите два числа через пробел: ").split())
+
+            print("-" * 18)
+            print("1. +")
+            print("2. -")
+            print("3. :")
+            print("4. x")
+            print("0. Выход")
+            command = int(input("Выберите действие: "))
+
+            if command == 0:
+                break
+            elif command == 1:
+                print(self.add(val1, val2))
+            elif command == 2:
+                print(self.subtract(val1, val2))
+            elif command == 3:
+                print(self.divide(val1, val2))
+            elif command == 4:
+                print(self.multiple(val1, val2))
